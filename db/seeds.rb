b221c6e5ded4997a58a30df0c00e6a6eea094c76
Nico Bazzoni <you@example.com>
2021-02-17 09:56:13 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,20 @@
-# Add seed data here. Seed your database with `rake db:seed`
+
+20.times do 
+  Artist.create()
+end
+​
+Genre.create(name: "rap")
+​
+Genre.create(name: "pop")
+​
+Genre.create(name: "country")
+​
+100.times do 
+  Song.create(artist_id: rand(1..20))
+end
+​
+100.times do
+  SongGenre.create(genres_id: rand(1..3), songs_id: rand(1..100))
+end
+
 
